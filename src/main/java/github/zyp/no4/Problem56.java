c2a598810fe62626145fd3094fe5892deacfb49b
zhangyp <13150337230@163.com>
2020-02-11 11:28:12 +0800

    feat: add 56.2 answer

--- src/main/java/github/zyp/no4/Problem56.java
+++ src/main/java/github/zyp/no4/Problem56.java
@@ -9,9 +9,42 @@ package github.zyp.no4;
  * 找到那个1的位置p，然后我们就可以根据第p位是否为1将所有的数字分成两堆，
  * 这样我们就把所有数字分成两部分，且每部分都是只包含一个只出现一次的数字、
  * 其他数字出现两次，从而将问题转化为最开始我们讨论的“数组中只出现一次的一个数字”问题。
+ *
+ * 题目2：数组中唯一只出现一次的数字
+ * 在一个数组中除了一个数字只出现一次外，其他数字都出现了三次
+ * 思路：
+ *  如果一个数字出现了三次，那么它二进制的每一位都出现了三次，所有出现三次的数字
+ *  的二进制表示的每一位都可以被3整除
+ *  我们把数组中没一个元素的二进制表示的每一位都加起来，如果某一位能被3整数，那么只
+ *  出现了一次的数字二进制表示就是0，否则是1
  */
 public class Problem56 {
 
+    public static void main(String[] args) {
+        int[] num = {4, 2, 2, 2, 5, 5, 5, 8, 8, 8};
+        System.out.println(findNumberAppearingOnce(num));
+    }
+
+    public static int findNumberAppearingOnce(int[] num) {
+        int[] bitSum = new int[32];
+        for (int n : num) {
+            int bitMask = 1;
+            for (int i = 31; i >= 0; i--) {
+                int bit = n & bitMask;
+                if (bit != 0)
+                    bitSum[i] += 1;
+                bitMask = bitMask << 1;
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result = result << 1;
+            result += bitSum[i] % 3;
+        }
+
+        return result;
+    }
     public static int findFirstBit1(int num) {
         if (num < 0)
             return -1;
